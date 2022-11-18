8add931e56b4256f589f6b51b7eb6ef6b5253431
uniquetraker <uniquetraker@gmail.com>
2022-11-18 23:34:19 +0530

    profile settings

--- app/src/main/java/com/wiesoftware/spine/ui/home/menus/profile/ProfileFragment.kt
+++ app/src/main/java/com/wiesoftware/spine/ui/home/menus/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.wiesoftware.spine.ui.home.menus.profile
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
@@ -50,23 +51,29 @@ import org.kodein.di.generic.instance
 import org.kodein.di.generic.kcontext
 
 
-class ProfileFragment : Fragment(),KodeinAware, ProfileFragmentEventListener {
+class ProfileFragment : Fragment(), KodeinAware, ProfileFragmentEventListener {
     override val kodein by kodein()
 
     override val kodeinContext = kcontext<Fragment>(this)
-    lateinit var binding:FragmentProfileBinding
+    lateinit var binding: FragmentProfileBinding
     lateinit var viewModel: ProfileFragmentViewModel
-    val factory : ProfileFragmentViewModelFactory by instance()
+    val factory: ProfileFragmentViewModelFactory by instance()
     val homeRepositry: HomeRepositry by instance()
     lateinit var user_id: String
-    var followers:String = "30k"
-    var following:String = "30k"
+    var followers: String = "30k"
+    var following: String = "30k"
+    lateinit var progressDialog: ProgressDialog
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
-        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_profile,container,false)
-        viewModel=ViewModelProvider(this,factory).get(ProfileFragmentViewModel::class.java)
-        binding.viewmodel=viewModel
-        viewModel.profileFragmentEventListener=this
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+        viewModel = ViewModelProvider(this, factory).get(ProfileFragmentViewModel::class.java)
+        binding.viewmodel = viewModel
+        viewModel.profileFragmentEventListener = this
+        progressDialog = ProgressDialog(context)
         setupViewPager()
         addTabs()
 
@@ -75,21 +82,22 @@ class ProfileFragment : Fragment(),KodeinAware, ProfileFragmentEventListener {
             if (user.facebook_id != null) {
                 val fbImg = getFbImage(user.facebook_id!!)
                 Log.e("fbImg:", fbImg)
-                Glide.with(requireContext()).asBitmap().load(fbImg).into(object : CustomTarget<Bitmap>() {
-                    override fun onResourceReady(
-                        resource: Bitmap,
-                        transition: Transition<in Bitmap>?
-                    ) {
-                        binding.circleImageView4.setImageBitmap(resource)
-                    }
+                Glide.with(requireContext()).asBitmap().load(fbImg)
+                    .into(object : CustomTarget<Bitmap>() {
+                        override fun onResourceReady(
+                            resource: Bitmap,
+                            transition: Transition<in Bitmap>?
+                        ) {
+                            binding.circleImageView4.setImageBitmap(resource)
+                        }
 
-                    override fun onLoadCleared(placeholder: Drawable?) {
-                        binding.circleImageView4.setImageDrawable(placeholder)
-                    }
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                            binding.circleImageView4.setImageDrawable(placeholder)
+                        }
 
-                })
+                    })
             }
-             getUsersDetails()
+            getUsersDetails()
         })
 
         return binding.root
@@ -98,49 +106,51 @@ class ProfileFragment : Fragment(),KodeinAware, ProfileFragmentEventListener {
     private fun getUsersDetails() {
         lifecycleScope.launch {
             try {
-                val profileRes=homeRepositry.getUserDetails()
-                if (profileRes.status){
-                    val profileData=profileRes.data
-                    BASE_IMAGE=profileRes.image
-                    if (!profileData.profile_pic.isNullOrEmpty()) {
-                        val url = profileData.profile_pic
+                showProgressDialog()
+                val profileRes = homeRepositry.getUserDetails()
+                if (profileRes.status) {
+                    dismissProgressDailog()
+                    val profileData = profileRes.data
+                    BASE_IMAGE = profileRes.image
+                    if (!profileData.user_image.isNullOrEmpty()) {
                         Glide.with(requireContext())
-                            .load(BASE_IMAGE + url)
-                            .placeholder(R.drawable.ic_spine_home)
+                            .load("https://thespiritualnetwork.com/assets/upload/profile/" + profileData.user_image)
+                            .placeholder(R.drawable.userprofile)
                             .into(binding.circleImageView4)
                     }
-                     followers=profileData.followers_records_count
-                     following=profileData.following_records_count
-                    binding.textView48.text=followers
-                    binding.textView49.text=following
-                    if (profileData.account_mode.equals("1")){
-                        binding.ivBadge.visibility=View.VISIBLE
-                    }else{
-                        binding.ivBadge.visibility=View.INVISIBLE
+                    followers = profileData.followers_records_count
+                    following = profileData.following_records_count
+                    binding.textView48.text = followers
+                    binding.textView49.text = following
+                    if (profileData.account_mode.equals("1")) {
+                        binding.ivBadge.visibility = View.VISIBLE
+                    } else {
+                        binding.ivBadge.visibility = View.INVISIBLE
                     }
 
                 }
-            }catch (e: ApiException){
+            } catch (e: ApiException) {
                 e.printStackTrace()
-            }catch (e: NoInternetException){
+            } catch (e: NoInternetException) {
                 e.printStackTrace()
-            }catch (e: Exception){
+            } catch (e: Exception) {
                 e.printStackTrace()
             }
         }
     }
 
-    private fun setupViewPager(){
-        val adapter= ProfileTabAdapter(this)
+    private fun setupViewPager() {
+        val adapter = ProfileTabAdapter(this)
         adapter.addFragment(PostsFragment())
         adapter.addFragment(EventsFragment())
         adapter.addFragment(PodcastFragment())
         adapter.addFragment(BookmarkFragment())
-        binding.viewpager2.offscreenPageLimit=4
-        binding.viewpager2.adapter=adapter
+        binding.viewpager2.offscreenPageLimit = 4
+        binding.viewpager2.adapter = adapter
     }
-    private fun addTabs(){
-        TabLayoutMediator(binding.tabLayout2,binding.viewpager2){tab, position ->
+
+    private fun addTabs() {
+        TabLayoutMediator(binding.tabLayout2, binding.viewpager2) { tab, position ->
         }.attach()
         binding.tabLayout2.getTabAt(0)?.text = resources.getText(R.string.post_value)
         binding.tabLayout2.getTabAt(1)?.text = resources.getText(R.string.events)
@@ -153,15 +163,15 @@ class ProfileFragment : Fragment(),KodeinAware, ProfileFragmentEventListener {
     }
 
     override fun onMessageBtnClick() {
-        startActivity(Intent(requireContext(),MessagesActivity::class.java))
+        startActivity(Intent(requireContext(), MessagesActivity::class.java))
     }
 
     override fun onViewProfileClick() {
-        startActivity(Intent(requireContext(),MyProfileActivity::class.java))
+        startActivity(Intent(requireContext(), MyProfileActivity::class.java))
     }
 
     override fun onFollowers() {
-        val intent = Intent(requireContext(),FollowActivity::class.java)
+        val intent = Intent(requireContext(), FollowActivity::class.java)
         intent.putExtra("followers", followers)
         intent.putExtra("following", following)
         startActivity(intent)
@@ -169,7 +179,7 @@ class ProfileFragment : Fragment(),KodeinAware, ProfileFragmentEventListener {
     }
 
     override fun onFollowing() {
-        val intent = Intent(requireContext(),FollowActivity::class.java)
+        val intent = Intent(requireContext(), FollowActivity::class.java)
         intent.putExtra("followers", followers)
         intent.putExtra("following", following)
         startActivity(intent)
@@ -197,17 +207,68 @@ class ProfileFragment : Fragment(),KodeinAware, ProfileFragmentEventListener {
             it.setBackgroundDrawableResource(android.R.color.transparent)
             dialog.setCancelable(true)
         }
-        dialog.button106.setOnClickListener { startActivity(Intent(requireContext(), PostThoughtActivity::class.java)) }
-        dialog.button107.setOnClickListener { startActivity(Intent(requireContext(), PostMediaActivity::class.java)) }
-        dialog.button108.setOnClickListener { startActivity(Intent(requireContext(), AddStoryActivity::class.java)) }
-        dialog.button109.setOnClickListener { startActivity(Intent(requireContext(), AddOrDupEventActivity::class.java))  }
-        dialog.button110.setOnClickListener { startActivity(Intent(requireContext(), AddRssActivity::class.java)) }
-        dialog.button111.setOnClickListener { startActivity(Intent(requireContext(),
-            FeaturedPostActivity::class.java)) }
+        dialog.button106.setOnClickListener {
+            startActivity(
+                Intent(
+                    requireContext(),
+                    PostThoughtActivity::class.java
+                )
+            )
+        }
+        dialog.button107.setOnClickListener {
+            startActivity(
+                Intent(
+                    requireContext(),
+                    PostMediaActivity::class.java
+                )
+            )
+        }
+        dialog.button108.setOnClickListener {
+            startActivity(
+                Intent(
+                    requireContext(),
+                    AddStoryActivity::class.java
+                )
+            )
+        }
+        dialog.button109.setOnClickListener {
+            startActivity(
+                Intent(
+                    requireContext(),
+                    AddOrDupEventActivity::class.java
+                )
+            )
+        }
+        dialog.button110.setOnClickListener {
+            startActivity(
+                Intent(
+                    requireContext(),
+                    AddRssActivity::class.java
+                )
+            )
+        }
+        dialog.button111.setOnClickListener {
+            startActivity(
+                Intent(
+                    requireContext(),
+                    FeaturedPostActivity::class.java
+                )
+            )
+        }
         dialog.show()
     }
 
+    override fun onResume() {
+        super.onResume()
+        getUsersDetails()
+    }
+    private fun showProgressDialog() {
+        progressDialog.setMessage("Please wait...")
+        progressDialog.setCancelable(false)
+        progressDialog.show()
+    }
 
-
-
+    private fun dismissProgressDailog() {
+        progressDialog.dismiss()
+    }
 }
